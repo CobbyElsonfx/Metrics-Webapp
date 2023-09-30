7440f9c5e4864a8259f232cfc7dfd40738c8b6fb
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-09-30 23:35:21 +0000

    implemented search funtionalituy

--- src/components/Region.jsx
+++ src/components/Region.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router';
 import { Link } from 'react-router-dom';
@@ -15,6 +15,32 @@ const Region = () => {
 
   const navigate = useNavigate();
   const { loading, data, error } = useSelector((state) => state.countries);
+
+  // State variables for search functionality
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredCountries, setFilteredCountries] = useState(data); // Initialize with original data
+  const [searchError, setSearchError] = useState(null); // State for search error
+
+  // Function to handle search input change
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+    setSearchError(null);
+  };
+
+  // Function to handle search button click
+  const handleSearchButtonClick = () => {
+    const inputValue = searchInput.trim().toLowerCase();
+    const filtered = data.filter((country) => country.name.common.toLowerCase() === inputValue);
+
+    if (filtered.length === 0) {
+      setSearchError('Country not found');
+      setFilteredCountries([]); // Clear filtered countries
+    } else {
+      setSearchError(null); // Clear search error
+      setFilteredCountries(filtered);
+    }
+  };
+
   return (
     <>
       <div>
@@ -22,37 +48,48 @@ const Region = () => {
           <button className="btn-icon-left" type="button" onClick={() => navigate(-1)}>
             <BsArrowLeftCircle className="icon-left" />
           </button>
-          <h2>
-            {region}
-          </h2>
+          <h2>{region}</h2>
         </header>
+        <div className="region-title">
+          <input
+            placeholder="Search for country"
+            className="inputSearch"
+            type="text"
+            value={searchInput}
+            onChange={handleSearchInputChange}
+          />
+          <button className="btnSearch" type="button" onClick={handleSearchButtonClick}>
+            Search
+          </button>
+        </div>
         {loading && <div className="loading">loading...</div>}
         {error && <div>error</div>}
+        {searchError && <div>{searchError}</div>}
+        {' '}
+        {/* Display search error */}
         <ul className="list-country">
-          {!loading && data?.map((country) => (
-            <li key={country.latlng[0]} className="list-item">
-              <Link to={`/${region}/${country.name.common}/${country.latlng[0]}/${country.latlng[1]}`}>
-                <div className="country">
-                  <img src={country.flags.png} alt={country.flags.alt} className="country-img" />
-                  <div>
-                    <BsArrowRightCircle />
-                    {' '}
+          {!loading
+            && filteredCountries.map((country) => (
+              <li key={country.latlng[0]} className="list-item">
+                <Link to={`/${region}/${country.name.common}/${country.latlng[0]}/${country.latlng[1]}`}>
+                  <div className="country">
+                    <img src={country.flags.png} alt={country.flags.alt} className="country-img" />
+                    <div>
+                      <BsArrowRightCircle />
+                    </div>
                   </div>
-                </div>
-                <div className="country-area">
-                  <span className="country-name">
-                    {country.name.common}
-                  </span>
-                  <span>
-                    {country.area}
-                    {' '}
-                    km
-                    <sup>2</sup>
-                  </span>
-                </div>
-              </Link>
-            </li>
-          ))}
+                  <div className="country-area">
+                    <span className="country-name">{country.name.common}</span>
+                    <span>
+                      {country.area}
+                      {' '}
+                      km
+                      <sup>2</sup>
+                    </span>
+                  </div>
+                </Link>
+              </li>
+            ))}
         </ul>
       </div>
     </>
